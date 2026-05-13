eb867da4d37b3c7573065ac29610af61aa14099c
devesh7yadav <devesh7yadav@gmail.com>
2026-05-13 19:55:20 -0400

    Added Two Sum

--- twoSum.java
+++ twoSum.java
@@ -1,3 +1,5 @@
+//Two Sum Solution
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
@@ -8,16 +10,5 @@ class Solution {
             }
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-
-        int[] result = sol.twoSum(nums, target);
-
-        System.out.println(result);
     }
 }
